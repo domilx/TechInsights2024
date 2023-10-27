7c5f145af4b9e17bc19c24c5d0d64ea9c305c778
domilx <domenico2727@gmail.com>
2023-10-27 14:45:57 -0400

    fixing modal

--- screens/components/FloatingButton.tsx
+++ screens/components/FloatingButton.tsx
@@ -1,23 +1,40 @@
 import React, { useState } from "react";
-import { View, TouchableOpacity, StyleSheet } from "react-native";
+import { View, TouchableOpacity, StyleSheet, Text, Modal } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import CameraScreen from "../CameraScreen";
 
 export default function FloatingButton() {
-  const [viewVisible, setViewVisible] = useState(false);
+  const [cameraVisible, setCameraVisible] = useState(false);
+  const [dataVisible, setDataVisible] = useState(false);
   const [scannedData, setScannedData] = useState<string | null>(null);
 
   const handleDataScanned = (data: string) => {
     setScannedData(data);
-    setViewVisible(false);
+    setCameraVisible(false); // Close the camera once data is scanned
+    setDataVisible(true); // Show the scanned data after camera is closed
+  };
+
+  const handleAddButtonClick = () => {
+    if (scannedData && !cameraVisible) {
+      setScannedData(null); // Reset the scanned data
+    }
+    setCameraVisible(!cameraVisible);
+    setDataVisible(false); // Hide scanned data when re-opening the camera
   };
 
   return (
     <>
-      {viewVisible && <CameraScreen onDataScanned={handleDataScanned} />}
-      
+      {cameraVisible && <CameraScreen onDataScanned={handleDataScanned} />}
+      <Modal visible={dataVisible} transparent={false} animationType="slide">
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text> {scannedData} </Text>
+          <TouchableOpacity onPress={() => setDataVisible(false)}>
+            <Text>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
       <View style={styles.floatingButton}>
-        <TouchableOpacity onPress={() => setViewVisible(!viewVisible)}>
+        <TouchableOpacity onPress={handleAddButtonClick}>
           <Icon name="add" size={30} color="#F6EB14" />
         </TouchableOpacity>
       </View>
@@ -36,6 +53,6 @@ const styles = StyleSheet.create({
     height: 60,
     justifyContent: "center",
     alignItems: "center",
-    zIndex: 1000,
+    zIndex: 2,
   }
 });
