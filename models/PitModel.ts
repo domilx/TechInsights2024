03096a7729b8d3d83aaed728c6497b3f6d7b9ae8
Domenico <domenico2727@gmail.com>
2023-12-06 10:16:21 -0500

    able to decode

---
+++ models/PitModel.ts
@@ -0,0 +1,84 @@
+export interface PitModel {  RobScout: string;
+    TeamNb: number;
+    RobTeamNm: string;
+    RobDrive: DriveBaseType;
+    RobMotor: DriveBaseMotor;
+    RobDriveExp: DriverExperience;
+    RobWtlbs: number;
+    RobWidth: number;
+    RobLength: number;
+    RobStble: Stability;
+    RobQuest1: boolean;
+    RobQuest2: boolean;
+    RobQuest3: boolean;
+    RobQuest4: boolean;
+    RobQuest5: boolean;
+    RobQuest6: boolean;
+    RobQuest7: boolean;
+    RobQuest8: boolean;
+    RobQuest9: boolean;
+    RobQuest10: boolean;
+    RobQuest11: boolean;
+    RobQuest12: boolean;
+    RobQuest13: boolean;
+    RobQuest14: boolean;
+    RobQuest15: boolean;
+    RobComm1: string;
+  }
+  
+  // Enums for dropdowns
+  export enum DriveBaseType {
+    Swerve = 'Swerve',
+    Tank = 'Tank',
+    Other = 'Other',
+  }
+  
+  export enum DriveBaseMotor {  CIM = 'CIM',
+    NEO = 'NEO',
+    FALCON = 'FALCON',
+    KRAKEN = 'KRAKEN',
+  }
+  
+  export enum DriverExperience {  Zero = 'Zero',
+    One = 'One',
+    Two = 'Two',
+    Three = 'Three',
+    Four = 'Four',
+    Unknown = 'Unknown',
+  }
+  
+  export enum Stability {  NO = 'Not Stable',
+    YES = 'Stable',
+    VERY_STABLE = 'Very Stable',
+  }
+  
+  
+  // Create an initial state object that matches the PitModel interface
+  export const initialPitData: PitModel = {  RobScout: "",
+    TeamNb: 0, 
+    RobTeamNm: "",
+    RobDrive: DriveBaseType.Other, // Default value as 'Other'
+    RobMotor: DriveBaseMotor.CIM, // Default value as 'CIM'
+    RobDriveExp: DriverExperience.Zero, // Default as 'Zero'
+    RobWtlbs: 0,
+    RobWidth: 0,
+    RobLength: 0,
+    RobStble: Stability.NO, // Default as 'NO'
+    RobQuest1: false,
+    RobQuest2: false,
+    RobQuest3: false,
+    RobQuest4: false,
+    RobQuest5: false,
+    RobQuest6: false,
+    RobQuest7: false,
+    RobQuest8: false,
+    RobQuest9: false,
+    RobQuest10: false,
+    RobQuest11: false,
+    RobQuest12: false,
+    RobQuest13: false,
+    RobQuest14: false,
+    RobQuest15: false,
+    RobComm1: "",
+  };
+  
--- screens/ConfirmDataScreen.tsx
+++ screens/ConfirmDataScreen.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { MatchModel } from '../models/MatchModel';
+import { PitModel } from '../models/PitModel';
 
 export default function ConfirmDataScreen({ route }: any) {
   const { data } = route.params;
+  
+  // Parse the JSON data
+  const parsedData = JSON.parse(data);
+
+  // Function to render MatchModel data
+  const renderMatchModel = (matchData: MatchModel) => {
+    return Object.entries(matchData).map(([key, value], index) => (
+      <Text style={styles.text} key={index}>{`${key}: ${value}`}</Text>
+    ));
+  };
+
+  // Function to render PitModel data
+  const renderPitModel = (pitData: PitModel) => {
+    return Object.entries(pitData).map(([key, value], index) => (
+      <Text style={styles.text} key={index}>{`${key}: ${value}`}</Text>
+    ));
+  };
+
+  // Check the type of model and render accordingly
+  const isMatchModel = parsedData.hasOwnProperty('ScoutName') || parsedData.hasOwnProperty('TeamNumber');
+  const content = isMatchModel
+    ? renderMatchModel(parsedData)
+    : renderPitModel(parsedData);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{data}</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      {content}
+    </ScrollView>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 20,
   },
   text: {
     fontSize: 16,
-    padding: 20,
+    marginBottom: 10,
   },
 });
